9680cb6103db992110a5d0ae7e7867527c2a8c5e
francois <francoisrvespa@gmail.com>
2016-02-11 12:31:28 -0600

    babel 6

--- dist/index.js
+++ dist/index.js
@@ -1,13 +1,14 @@
 'use strict';
 
-Object.defineProperty(exports, '__esModule', {
+Object.defineProperty(exports, "__esModule", {
   value: true
 });
+exports.Row = undefined;
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
+var _row = require('./components/row');
 
-var _componentsRow = require('./components/row');
+var _row2 = _interopRequireDefault(_row);
 
-var _componentsRow2 = _interopRequireDefault(_componentsRow);
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-exports.Row = _componentsRow2['default'];
+exports.Row = _row2.default;
